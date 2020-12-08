dfabf82beed8c3e1ea4bae1414cf9605137a2396
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-12-08 16:47:16 +0000

    Test updated.

--- BookmarkingApp/test/dev/umairashraf/BookmarkingApp/entities/MovieTest.java
+++ BookmarkingApp/test/dev/umairashraf/BookmarkingApp/entities/MovieTest.java
@@ -11,14 +11,14 @@ public class MovieTest {
     public void testIskidFriendlyEligible() {
         Movie movie = BookmarkManager.getInstance().createMovie(3000, "", "   Citizen Kane", 1941,
             new String[] { "    Orson Welles,Joseph Cotten" }, new String[] { "    Orson Welles" },
-            MovieGenre.CLASSICS, 8.5);
+            MovieGenre.HORROR, 8.5);
         boolean iskidFriendlyEligible = movie.iskidFriendlyEligible();
         assertFalse("For Horror Genere-isKidFriendlyEligible should return false",
             iskidFriendlyEligible);
         
           movie = BookmarkManager.getInstance().createMovie(3000, "", "   Citizen Kane", 1941,
             new String[] { "    Orson Welles,Joseph Cotten" }, new String[] { "    Orson Welles" },
-            MovieGenre.CLASSICS, 8.5);
+            MovieGenre.THRILLERS, 8.5);
           iskidFriendlyEligible = movie.iskidFriendlyEligible();
         assertFalse("For THRILLER Genere-isKidFriendlyEligible should return false",
             iskidFriendlyEligible);
