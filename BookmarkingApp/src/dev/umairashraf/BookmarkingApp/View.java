6d9f1c5745b78b8127722882309dc5a6fcdf6dc8
rock-007 <59394025+rock-007@users.noreply.github.com>
2020-12-08 12:11:02 +0000

    Formatted

---
+++ BookmarkingApp/src/dev/umairashraf/BookmarkingApp/View.java
@@ -0,0 +1,11 @@
+package dev.umairashraf.BookmarkingApp;
+
+import dev.umairashraf.BookmarkingApp.entities.Bookmark;
+import dev.umairashraf.BookmarkingApp.entities.User;
+
+public class View {
+
+    public static void bookmark(User user, Bookmark[][] bookmarks) {
+
+    }
+}
